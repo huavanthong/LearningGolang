dd71f48b248cdf8891713a9fd725262da4e77718
huavanthong <50081052+huavanthong@users.noreply.github.com>
2022-01-06 09:43:49 +0700

    Update main.go

--- GolangProject/src/webserver/main.go
+++ GolangProject/src/webserver/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
 
-	fmt.Printf("Hello World\n")
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello go!"))
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+
 }
