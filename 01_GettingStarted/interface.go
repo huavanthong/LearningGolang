aa9a9ef786bb2a0ad4922d7b11189b87813da64a
huavanthong <50081052+huavanthong@users.noreply.github.com>
2022-02-08 11:57:04 +0700

    Update methods to get/set member in structure Go

--- 01_GettingStarted/interface.go
+++ 01_GettingStarted/interface.go
@@ -5,17 +5,25 @@ import (
 	"math"
 )
 
+/*************************************************************************************************/
+/* Parent structure using interface
+/*************************************************************************************************/
 // Go Interface - `Shape`
 type Shape interface {
 	Area() float64
 	Perimeter() float64
 }
 
+/*************************************************************************************************/
+/* Rectangle will be the Shape'children if it implements all Shape's methods
+/*************************************************************************************************/
 // Struct type `Rectangle` - implements the `Shape` interface by implementing all its methods.
 type Rectangle struct {
 	Length, Width float64
 }
 
+/********* Methods inherit *********/
+// Methods inherit from Shape
 func (r Rectangle) Area() float64 {
 	return r.Length * r.Width
 }
@@ -24,11 +32,33 @@ func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Length + r.Width)
 }
 
+/********* Methods utility *********/
+// Set width by using pointer
+func (r *Rectangle) setWidth(width float64) {
+	(*r).Width = width
+}
+
+// Set length by using pointer
+func (r *Rectangle) setLength(length float64) {
+	(*r).Length = length
+}
+
+// show info by using value
+func (r Rectangle) show() {
+	fmt.Println("Length : ", r.Length)
+	fmt.Println("Width  : ", r.Width)
+}
+
+/*************************************************************************************************/
+/* Circle will be the Shape'children if it implements all Shape's methods
+/*************************************************************************************************/
 // Struct type `Circle` - implements the `Shape` interface by implementing all its methods.
 type Circle struct {
 	Radius float64
 }
 
+/********* Methods inherit *********/
+// Methods inherit from Shape
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
@@ -37,8 +67,19 @@ func (c Circle) Perimeter() float64 {
 	return 2 * math.Pi * c.Radius
 }
 
+/********* Methods extend *********/
+// Methods extend
 func (c Circle) Diameter() float64 {
 	return 2 * c.Radius
+}
+
+/********* Methods utility *********/
+func (c *Circle) setRadius(radius float64) {
+	(*c).Radius = radius
+}
+
+func (c Circle) show() {
+	fmt.Println("Radius : ", c.Radius)
 }
 
 // Generic function to calculate the total area of multiple shapes of different types
@@ -66,4 +107,11 @@ func main() {
 
 	totalArea := CalculateTotalArea(Circle{2}, Rectangle{4, 5}, Circle{10})
 	fmt.Println("Total area = ", totalArea)
+	fmt.Println()
+
+	var rect Rectangle
+	rect.setLength(10)
+	rect.setWidth(12)
+	fmt.Printf("Rect Type = %T, Rhape Value = %v\n", rect, rect)
+	fmt.Printf("Area = %f, Perimeter = %f\n", rect.Area(), rect.Perimeter())
 }
