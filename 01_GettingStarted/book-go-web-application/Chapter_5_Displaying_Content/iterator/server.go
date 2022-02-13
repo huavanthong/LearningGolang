35b96b9e40be2e198a917207425281067f7ea915
Thong Hua <huavanthong14@gmail.com>
2022-02-13 13:38:57 +0700

    [Iterator] Solution to loop maps in Golang

--- 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/iterator/server.go
+++ 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/iterator/server.go
@@ -11,10 +11,28 @@ func process(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, daysOfWeek)
 }
 
+// Create map info and pass to template
+func process3(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl3.html")
+	// Create slice of string to contain all days
+	var countryCapitalMap map[string]string
+	/* create a map*/
+	countryCapitalMap = make(map[string]string)
+
+	/* insert key-value pairs in the map*/
+	countryCapitalMap["France"] = "Paris"
+	countryCapitalMap["Italy"] = "Rome"
+	countryCapitalMap["Japan"] = "Tokyo"
+	countryCapitalMap["India"] = "New Delhi"
+
+	t.Execute(w, countryCapitalMap)
+}
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/process3", process3)
+
 	server.ListenAndServe()
 }
