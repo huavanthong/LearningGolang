73a62e98c88b7d705a64aed86c228e53f86d4c13
Thong Hua <huavanthong14@gmail.com>
2022-02-13 13:43:55 +0700

    [Origin] Update format for simpel in book

--- 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/iterator/server.go
+++ 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/iterator/server.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
+/************************************************************************************************
+This is a example to show how to create using contional actions in 5.3.2 book go-web-application.
+This function will show you how to pass array string to template
+************************************************************************************************/
+// Create slice one dimensional
 func process(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("tmpl.html")
+	// Create slice of string to contain all days
 	daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+	for day := range daysOfWeek {
+		fmt.Println(day, daysOfWeek[day])
+	}
+	//Template
+	/*
+		<ul>
+		{{ range . }}
+		  <li>{{ . }}</li>
+		{{ end}}
+		</ul>
+	*/
 	t.Execute(w, daysOfWeek)
 }
 
@@ -59,6 +77,7 @@ func process3(w http.ResponseWriter, r *http.Request) {
 
 	t.Execute(w, countryCapitalMap)
 }
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
