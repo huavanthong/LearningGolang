009fdcf7c9487b543a9c25efb1651983fc8798fb
Thong Hua <huavanthong14@gmail.com>
2022-02-13 14:21:05 +0700

    [Include-Action] Pass arguement to template 2.

--- 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/include/server.go
+++ 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/include/server.go
@@ -10,10 +10,16 @@ func process(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, "Hello World!")
 }
 
+func process1(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("t1_pass_arg_to_t2.html", "t2.html")
+	t.Execute(w, "Hello World!")
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/process1", process1)
 	server.ListenAndServe()
 }
