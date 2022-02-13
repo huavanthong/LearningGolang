526960b018eaf73aa00183bef49fc708c709ea90
Thong Hua <huavanthong14@gmail.com>
2022-02-13 14:44:39 +0700

    [Function in Golang] Update knowledge for function in golang.

--- 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/custom_function/server.go
+++ 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/custom_function/server.go
@@ -6,15 +6,21 @@ import (
 	"time"
 )
 
+// Create a function for template
 func formatDate(t time.Time) string {
 	layout := "2006-01-02"
 	return t.Format(layout)
 }
 
+// API to map the internal function
 func process(w http.ResponseWriter, r *http.Request) {
+	// Mapping to internal function by using package template and FuncMap() method
 	funcMap := template.FuncMap{"fdate": formatDate}
+	// From template, we create Funcs for it, using Funcs() method
 	t := template.New("tmpl.html").Funcs(funcMap)
+	// Start to parse html
 	t, _ = t.ParseFiles("tmpl.html")
+	// Start to execute.
 	t.Execute(w, time.Now())
 }
 
