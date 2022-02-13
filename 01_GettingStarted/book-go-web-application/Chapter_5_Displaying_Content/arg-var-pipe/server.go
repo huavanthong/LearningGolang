056f733d9f02c648bed4c94cbfce01ff7135dad1
Thong Hua <huavanthong14@gmail.com>
2022-02-13 13:34:08 +0700

    [Variable] Make solution to pass multiple variable to template.

--- 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/arg-var-pipe/server.go
+++ 01_GettingStarted/book-go-web-application/Chapter_5_Displaying_Content/arg-var-pipe/server.go
@@ -28,15 +28,19 @@ func process2(w http.ResponseWriter, r *http.Request) {
 }
 
 // Pass multiple varialbe to template
+// Experience that, we should create all varibles by map instead of one variable
+// Refer: https://stackoverflow.com/questions/27971240/how-to-pass-just-a-variable-not-a-struct-member-into-text-html-template-golan
 func process3(w http.ResponseWriter, r *http.Request) {
+
 	t, _ := template.ParseFiles("tmpl3.html")
 
-	var index int = 10
-	ptr_to_index := new(int)
-	ptr_to_index = &index
+	varmap := map[string]interface{}{
+		"index": 100,
+		"info":  "This is a info at index 100",
+	}
 
 	// var info string = "I'm a value at index"
-	t.Execute(w, ptr_to_index)
+	t.Execute(w, varmap)
 	// t.Execute(w, info)
 }
 
@@ -52,6 +56,7 @@ func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
+	http.HandleFunc("/index", index)
 	http.HandleFunc("/process", process)
 	http.HandleFunc("/process2", process2)
 	http.HandleFunc("/process3", process3)
