6fcc3b21a562afe8516419980bfb819d2cb013ff
Thong Hua <huavanthong14@gmail.com>
2022-03-15 23:22:55 +0700

    [Timeout] Update sample to using timeout in server configuration.

---
+++ 01_GettingStarted/book-go-web-application/Chapter_3_Handling_Requests/config_timeout/server_timeout.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+var layout *template.Template
+var WriteTimeout = 1 * time.Second
+
+func main() {
+	router := http.NewServeMux()
+	server := &http.Server{
+		Addr:         ":8889",
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: WriteTimeout + 10*time.Millisecond, //10ms Redundant time
+		IdleTimeout:  15 * time.Second,
+	}
+	router.HandleFunc("/", home)
+	server.ListenAndServe()
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("responding\n")
+	ctx, _ := context.WithTimeout(context.Background(), WriteTimeout)
+	worker, cancel := context.WithCancel(context.Background())
+	var buffer string
+	go func() {
+		// do something
+		time.Sleep(2 * time.Second)
+		buffer = "ready all response\n"
+		//do another
+		time.Sleep(2 * time.Second)
+		cancel()
+		fmt.Printf("worker finish\n")
+	}()
+	select {
+	case <-ctx.Done():
+		//add more friendly tips
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	case <-worker.Done():
+		w.Write([]byte(buffer))
+		fmt.Printf("writed\n")
+		return
+	}
+}
