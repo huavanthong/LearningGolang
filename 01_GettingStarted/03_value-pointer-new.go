7e9e616959b87453b0855bd11ba91853667b7ad9
huavanthong <50081052+huavanthong@users.noreply.github.com>
2022-03-22 09:04:06 +0700

    Create method using allocation with new as a pointer

--- 01_GettingStarted/03_value-pointer-new.go
+++ 01_GettingStarted/03_value-pointer-new.go
@@ -12,8 +12,13 @@ func main() {
 
 	fmt.Println("==== Method 1: Create structure variable ====")
 	var test Student
-	fmt.Printf("Struct Variable: Check value %d\n", test.Id)
-	fmt.Printf("Struct Variable: Check address %p\n", &test)
+	test.Id = 1
+	test.Name = "hvthong"
+	test.Subject = "math"
+	fmt.Printf("Struct Variable: Check Id: %d\n", test.Id)
+	fmt.Printf("Struct Variable: Check Name: %s\n", test.Name)
+	fmt.Printf("Struct Variable: Check Subject: %s\n", test.Subject)
+	fmt.Printf("Struct Variable: Check address: %p\n", &test)
 
 	fmt.Println("==== Method 2: Create structure pointer ====")
 	var ptr_test *Student = &test
@@ -34,4 +39,11 @@ func main() {
 	p := new(Student)
 	fmt.Printf("Struct new: Check value %d\n", p.Id)
 	fmt.Printf("Struct new: Check address %p\n", p)
+
+	fmt.Println("==== Method 5: Consider using allocation with new as a pointer ====")
+	p = &test
+	fmt.Printf("Struct Variable: Check Id: %d\n", p.Id)
+	fmt.Printf("Struct Variable: Check Name: %s\n", p.Name)
+	fmt.Printf("Struct Variable: Check Subject: %s\n", p.Subject)
+	fmt.Printf("Struct Variable: Check address: %p\n", p)
 }
