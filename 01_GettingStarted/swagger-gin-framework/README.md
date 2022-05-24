07b4ef193d636944638ca4d8db17a7576fadf27a
hvthong <huavanthong14@gmail.com>
2022-05-24 14:36:15 +0700

    [Swagger][Gin][Document] Update document for swagger.

--- 01_GettingStarted/swagger-gin-framework/README.md
+++ 01_GettingStarted/swagger-gin-framework/README.md
@@ -4,9 +4,29 @@
 1. Example about swagger with gin framework. Refer: [here](https://levelup.gitconnected.com/tutorial-generate-swagger-specification-and-swaggerui-for-gin-go-web-framework-9f0c038483b5)
 
 
-# Getting Started
+## Getting Started
 To run this app
 ```
 go run .\ginsimple\main.go
 ```
 
+## Swagger
+### Install 
+Install swagger for documentation
+```
+go get -v github.com/swaggo/swag/cmd/swag
+go get -v github.com/swaggo/gin-swagger
+go get -v github.com/swaggo/files
+```
+### Generate
+Generate document by swagger.
+
+* **Windows:**
+```
+.\swagger.bat init -g ginsimple/main.go --output docs/ginsimple
+```
+
+* **Linux:**
+```
+swag init -g ginsimple/main.go --output docs/ginsimple
+```
--- 01_GettingStarted/swagger-gin-framework/ginsimple/main.go
+++ 01_GettingStarted/swagger-gin-framework/ginsimple/main.go
@@ -7,6 +7,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @title Gin Swagger Example API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /
+// @schemes http
 func main() {
 	// Gin instance
 	r := gin.New()
@@ -20,6 +35,14 @@ func main() {
 	}
 }
 
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
 func HealthCheck(c *gin.Context) {
 	res := map[string]interface{}{
 		"data": "Server is up and running",
