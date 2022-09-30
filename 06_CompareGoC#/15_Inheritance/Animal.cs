d7e50a9c0b5934da9630449a48ceb63f31343981
hvthong <huavanthong14@gmail.com>
2022-09-30 13:22:04 +0700

    [C#] Override method, Overloading method.

--- 06_CompareGoC#/15_Inheritance/Animal.cs
+++ 06_CompareGoC#/15_Inheritance/Animal.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Inheritance {
 
+    /******************************************************************************
+    ***************************** Grandparent class *******************************
+    ******************************************************************************/
     public class Animals {
         private string gender {get; set;}
         private int age {get; set;}
@@ -23,27 +26,60 @@ namespace Inheritance {
         public void Sleep() {
             Console.WriteLine($"Sleep:");
         }
-        public void Eat()
-        {
+
+        public void Eat() {
             Console.WriteLine($"Eat: ");
         }
-        public void Move()
-        {
+
+        public void Move() {
             Console.WriteLine($"Move: ");
         }
 
+        // If you want define a method that can create a object 
+        // and child class can override this method
+        // ===> use virtual
+        public virtual void MakeNoise() { 
+            Console.WriteLine($"Make some noise: ");
+        }
+
+        // If you want define a method that don't create a object 
+        // and all child classes must override this method
+        // ===> use abstract
+        // public abstract void Action();
+
         // Method to use attribute
-        public void ShowAge()
+        public void ShowInfo()
         {
-            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"This aminal: Gender = {gender}; Age = {age}; Weigth = {weigth} ");
+        }
+
+        // Implement method overloading
+        public void ShowInfo(string act)
+        {
+            Console.WriteLine($"This aminal: Gender = {gender}; Age = {age}; Weigth = {weigth} ");
+            
+            if(String.Equals(act, "sleep")) {
+                Sleep();
+            } else if(String.Equals(act, "eat")) {
+                Eat();
+            } else if(String.Equals(act, "move")) {
+                Move();  
+            } else if(String.Equals(act, "all")) {
+                Sleep();
+                Eat();
+                Move();
+            } else {
+                Console.WriteLine($"Please choose action: sleep, eat, move, all");
+            }
         }
     } 
 
-    /************* Inheritance Mammal************/
-    public class Mammal : Animals {
-        public int Legs {get; set;} // Add a new attribute
 
-        // Avoid error: The type `Inheritance.Mammal' does not contain a constructor that takes `0' arguments
+    /************************** Inheritance Mammal: Parent class *******************************/
+    public class Mammal : Animals {
+        protected int Legs {get; set;} // Add a new attribute
+
+        // Avoid error 1: The type `Inheritance.Mammal' does not contain a constructor that takes `0' arguments
         // Must implement this constructor
         public Mammal() {
 
@@ -59,6 +95,7 @@ namespace Inheritance {
         }
     }
 
+    /************* Cat class: Child class ************/
     public class Cat : Mammal {
         public string food;
         public Cat() {
@@ -72,36 +109,57 @@ namespace Inheritance {
         }
     }
 
-    /************* Inheritance Bird ************/
+    /************************** Inheritance Bird: Parent class *******************************/
+
     public class Bird : Animals {
         public string color; // thuộc tính mới thêm
         public Bird() {
             color = "Yellow";
         }
+
+        // Override virtual method 
+        public override void MakeNoise() {
+            // Before override method, we can call virtual method behavior
+            // Call virtual from base class to get some nose.
+            base.MakeNoise();
+
+            // Redefine method 
+            Console.WriteLine($"Bird sing ");
+        }
     }
 
-    /************* Inheritance Reptile************/
+    /************************** Inheritance Reptile: Parent class *******************************/
     public class Reptile : Animals {
  
     }
 
 
-    /************* Inheritance Fish************/
-
+    /************************** Inheritance Fish: Parent class *******************************/
     public class Fish : Animals {
 
     }
 
+    /******************************************************************************
+    ********************************** Program ************************************
+    ******************************************************************************/
     class Program {
         static void Main (string[] args) {
-            /* Ví dụ kế thừa */    
+            /* Example 1: Inheritance */    
             Cat cat = new Cat();
-            cat.ShowAge (); // Phương thức này kế thừa từ lớp cơ sở
+            cat.ShowInfo(); // Phương thức này kế thừa từ lớp cơ sở
+            cat.ShowLegs();
             cat.SetLegs(3);
             cat.ShowLegs();
-            cat.Eat (); // phương thức của riêng Cat
+            cat.Eat(); // phương thức của riêng Cat
+            cat.ShowInfo("sleep");
 
-        }
+
+            /* Example 2: Override method */    
+            Bird bird = new Bird();
+            bird.MakeNoise();
+
+            /* Example 3: Abstract method */
+        }       
 
     }
 }
