98af5ebc567fcbaade744bf56a687bcc10267ef6
hvthong <hvthong@cmcglobal.vn>
2022-11-14 17:50:23 +0700

    Restructure to microservice.

--- 03_ReferenceProject/09_docker-compose-go-app/database/database.go
+++ 03_ReferenceProject/09_docker-compose-go-app/database/database.go
@@ -1,10 +1,41 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+// DB is the pointer to the
+// database connection
 var (
 	DBConn *gorm.DB
 )
+
+// InitDatabase returns a pointer to a database connection
+// calling functions need to ensure they defer the closing
+// of this connection.
+func InitDatabase() error {
+	var err error
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_TABLE")
+	dbPort := os.Getenv("DB_PORT")
+
+	// dbConnectionString := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" +strconv
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	fmt.Println(connectionString)
+	DBConn, err = gorm.Open("postgres", connectionString)
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Database connection successfully opened")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+
+}
--- 03_ReferenceProject/09_docker-compose-go-app/main.go
+++ 03_ReferenceProject/09_docker-compose-go-app/main.go
@@ -1,46 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/huavanthong/MasterGolang/tree/main/03_ReferenceProject/09_docker-compose-go-app/database"
+	"github.com/huavanthong/MasterGolang/tree/main/03_ReferenceProject/09_docker-compose-go-app/transport"
 
-	"github.com/elliotforbes/go-fiber-tutorial/book"
-	"github.com/elliotforbes/go-fiber-tutorial/database"
-	"github.com/gofiber/fiber"
-
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func helloWorld(c *fiber.Ctx) {
-	c.Send("Hello, World!")
-}
-
-func setupRoutes(app *fiber.App) {
-	app.Get("/api/v1/book", book.GetBooks)
-	app.Get("/api/v1/book/:id", book.GetBook)
-	app.Post("/api/v1/book", book.NewBook)
-	app.Delete("/api/v1/book/:id", book.DeleteBook)
-}
-
-func initDatabase() {
-	var err error
-	database.DBConn, err = gorm.Open("sqlite3", "books.db")
-	if err != nil {
-		panic("Failed to connect to database")
-	}
-	fmt.Println("Database connection successfully opened")
-
-	database.DBConn.AutoMigrate(&book.Book{})
-	fmt.Println("Database Migrated")
-
-}
-
 func main() {
-	app := fiber.New()
-	initDatabase()
-	defer database.DBConn.Close()
+	app := transport.SetUp()
+	err := database.InitDatabase()
+	if err != nil {
+		panic(err)
+	}
 
-	setupRoutes(app)
-
-	app.Listen(3000)
+	app.Listen(5000)
 }
